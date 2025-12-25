b4c1918d9cb92d8d01ba94bc28e3c34c1641852b
BhargaviSisode <bhargavisisode1608@gmail.com>
2025-12-25 14:52:46 +0530

    Implement navbar style change on scroll
    Add scroll event listener to change navbar style

---
+++ script.js
@@ -0,0 +1,9 @@
+// Change navbar style when scrolling
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
